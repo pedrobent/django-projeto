3217a18e6cc75991163e0ba3b9a7e2943d75d077
pedrobent <pedrobento131@gmail.com>
2023-03-20 19:00:39 -0300

    template

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/pages/home.html')
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
+
     
