3c2f8072ed7f36e3833b2cb2312099f1a92e117b
pedrobent <pedrobento131@gmail.com>
2023-03-21 14:57:51 -0300

    teste

--- recipes/views.py
+++ recipes/views.py
@@ -4,10 +4,13 @@ from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html',context={'recipes': [make_recipe() for _ in range(10)],
-})
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html',context={'recipe': make_recipe(),
-})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
