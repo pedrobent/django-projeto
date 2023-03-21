105cda9145fa01eaf53b26eb1dda0813ee7ab657
pedrobent <pedrobento131@gmail.com>
2023-03-21 13:08:30 -0300

    teste

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html',context={'recipes': [make_recipe() for _ in range(10)],
+})
+
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html',context={'recipe': make_recipe(),
+})
