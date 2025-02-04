d915a3b9c260bbcae9bbe5a02e7d458018d1e141
Pedro Bento <111144417+pedrobent@users.noreply.github.com>
2025-02-03 21:02:20 -0300

    Update models.py

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
