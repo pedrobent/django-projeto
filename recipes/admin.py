17c08709081504866c7ce93a3dc0f5b85ca425e9
Pedro Bento <111144417+pedrobent@users.noreply.github.com>
2025-02-04 10:50:53 -0300

    Update admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
 
 class categoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
     ...
 
 admin.site.register(Category, categoryAdmin)
