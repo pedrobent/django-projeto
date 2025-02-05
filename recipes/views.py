c29fe298a72951e133bdb0d4905b4d731e596999
Pedro Bento <111144417+pedrobent@users.noreply.github.com>
2025-02-05 20:54:26 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -5,14 +5,19 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published = True
+        ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id = category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id = category_id,
+        is_published = True
+        ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
