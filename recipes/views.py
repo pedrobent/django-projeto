0d6a5317bf2adc193b1739d7de7e38c7a6dd2d78
Pedro Bento <111144417+pedrobent@users.noreply.github.com>
2025-02-05 12:32:47 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
