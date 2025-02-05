d0b146e4e70deab79bee206605ef7eae3a32d8c8
Pedro Bento <111144417+pedrobent@users.noreply.github.com>
2025-02-05 12:32:50 -0300

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe")
+    path('recipes/category/<int:category_id>/', views.category, name="category")
 ]
