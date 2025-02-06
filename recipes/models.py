601c0dc0d93948fb89fb89fd143adc0359c7c341
Pedro Bento <111144417+pedrobent@users.noreply.github.com>
2025-02-05 21:42:25 -0300

    Update models.py

--- recipes/models.py
+++ recipes/models.py
@@ -22,11 +22,11 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%y/%m/%d/',blank=True,default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True,default= None,
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default= None,
     )
 
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True
+        User, on_delete=models.SET_NULL, null=True, blank=True, default= None,
     )
 
     def __str__(self):
