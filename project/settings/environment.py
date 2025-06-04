d69d414b39fb78f0a03ecf11ddf0e3551d0523fc
pedro <pedrobento131@gmail.com>
2025-06-04 12:48:11 -0300

    settings https

--- project/settings/environment.py
+++ project/settings/environment.py
@@ -17,9 +17,9 @@ DEBUG = True if os.environ.get('DEBUG') == '1' else False
 ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
     get_env_variable('ALLOWED_HOSTS')
 )
-# CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
-#     get_env_variable('CSRF_TRUSTED_ORIGINS')
-# )
+CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
+)
 
 ROOT_URLCONF = 'project.urls'
 
