b72899d8dbd5d6ae9fa4f74a04a74dc6c10ecc5b
pedro <pedrobento131@gmail.com>
2025-06-12 22:38:59 -0300

    atualizando js

--- base_static/global/js/project.js
+++ base_static/global/js/project.js
@@ -5,3 +5,8 @@ next.addEventListener('click', function(){
     let items = document.querySelectorAll('.item-project')
     document.querySelector('.slide').appendChild(items[0])
 })
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
