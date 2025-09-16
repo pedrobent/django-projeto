e383344a244a41202f13c2a82187e257cd354e1e
pedro <pedrobento131@gmail.com>
2025-09-16 00:35:34 -0300

    assets

--- project/settings/assets.py
+++ project/settings/assets.py
@@ -1,14 +1,13 @@
 from .environment import BASE_DIR
-import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'base_static',
 ]
-STATIC_ROOT = os.path.join('/etc/pedro/app_repo', 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
